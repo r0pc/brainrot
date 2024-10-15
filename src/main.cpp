70c0bac0dc679f02bba114c0c605abf83fec644b
mr0pc <mropc@archmropc.localdomain>
2024-10-15 18:33:33 +0500

    2nd

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 
-int main(void){
-  std::cout<<"helloworld"<<std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cerr << "No File Provided" << endl;
+    return EXIT_FAILURE;
+  }
+
+
+
   return 0;
 }
